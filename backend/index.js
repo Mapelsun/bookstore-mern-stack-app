5e41ab86f91a1d6e9c0ca5f1e0404c775fcf4ae4
Pels <pelumisunday@gmail.com>
2023-09-03 18:05:59 +0100

    fix: cors error with cors as dep

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,22 @@ import express from 'express'
 import { PORT, mongoDBURL } from './config.js'
 import mongoose from 'mongoose'
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
 const app = express()
 
 // Middleware for parsing request body
 app.use(express.json())
+
+// Allow all origins with default of cors(*)
+app.use(cors())
+
+// // Allow custom origins
+// app.use({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type'],
+// })
 
 app.get('/', (req, res) => {
   console.log(req)
